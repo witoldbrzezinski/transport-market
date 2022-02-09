53acd01b89a052bb105031bc682b3374db176128
Witold Brzezinski <witbrzezinski@gmail.com>
2022-02-09 20:13:07 +0100

    starter project with spring intializr

---
+++ src/main/java/pl/witoldbrzezinski/transportmarket/TransportmarketApplication.java
@@ -0,0 +1,13 @@
+package pl.witoldbrzezinski.transportmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/witoldbrzezinski/transportmarket/TransportmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.witoldbrzezinski.transportmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
