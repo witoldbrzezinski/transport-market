1d0fc3713488c9a1434d9ca9770df7df798745f2
Witold Brzezinski <witbrzezinski@gmail.com>
2022-02-14 18:57:01 +0100

    connected with postres db

---
+++ src/main/resources/sql/customer-db-create.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers
+(
+	id SERIAL PRIMARY KEY,
+	name CHARACTER VARYING(255),
+	country CHARACTER VARYING(100)
+);
+
+
+INSERT INTO customers
+VALUES (1, 'Optima', 'Poland'),
+       (2, 'DSV', 'Denmark');
+
