3ac4c5255e7907470d67e31e3b5f52c7cfec0336
Witold Brzezinski <witbrzezinski@gmail.com>
2022-02-23 13:23:15 +0100

    created reusable navbar

--- src/main/java/pl/witoldbrzezinski/transportmarket/controller/LoadController.java
+++ src/main/java/pl/witoldbrzezinski/transportmarket/controller/LoadController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import pl.witoldbrzezinski.transportmarket.entity.Load;
 import pl.witoldbrzezinski.transportmarket.service.LoadService;
@@ -16,7 +16,7 @@ public class LoadController {
 	@Autowired
 	LoadService service;
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index(Model model) {
 		
 		List<Load> loadList = service.getAllLoads();
@@ -26,5 +26,9 @@ public class LoadController {
 		return "index.html";
 	}
 	
+	@GetMapping("/addNewLoad")
+	public String addLoad() {
+		return "new-load.html";
+	}
 
 }
