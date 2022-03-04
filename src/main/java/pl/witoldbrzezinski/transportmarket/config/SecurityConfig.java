cedef044a237113e2454a8072f773e12a5d93978
Witold Brzezinski <witbrzezinski@gmail.com>
2022-03-04 20:51:08 +0100

    added custom login page

--- src/main/java/pl/witoldbrzezinski/transportmarket/config/SecurityConfig.java
+++ src/main/java/pl/witoldbrzezinski/transportmarket/config/SecurityConfig.java
@@ -4,17 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests()
-				.mvcMatchers(HttpMethod.GET, "/").permitAll()
+			http
+			.csrf().disable()
+			.authorizeRequests()
+				.mvcMatchers(HttpMethod.GET, "/").permitAll()	
 				 .anyRequest().authenticated()
 				 .and()
+				 .formLogin()
+			     .loginPage("/login").permitAll()
+//			     .loginProcessingUrl("/login")
+			     .defaultSuccessUrl("/", true)
+//			     .failureUrl("/login.html?error=true")
+			     .and()
 				 .httpBasic();
 	}
 
---
+++ src/main/java/pl/witoldbrzezinski/transportmarket/controller/LoginController.java
@@ -0,0 +1,17 @@
+package pl.witoldbrzezinski.transportmarket.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public String login() {
+		
+		return "login.html";
+	}
+
+}
