a8c0c23a168499bf3ceeb779420f26ea0550e95a
Witold Brzezinski <witbrzezinski@gmail.com>
2022-04-12 17:34:18 +0200

    added some tests part 1

---
+++ src/test/java/pl/witoldbrzezinski/transportmarket/WebLayerTest.java
@@ -0,0 +1,49 @@
+package pl.witoldbrzezinski.transportmarket;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import pl.witoldbrzezinski.transportmarket.entity.Load;
+import pl.witoldbrzezinski.transportmarket.service.CustomerService;
+import pl.witoldbrzezinski.transportmarket.service.LoadService;
+import pl.witoldbrzezinski.transportmarket.service.UserService;
+
+@WebMvcTest
+@AutoConfigureTestDatabase
+public class WebLayerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	CustomerService customerService;
+	@MockBean
+	LoadService loadService;
+	@MockBean
+	UserService userService;
+	
+	@Test
+	@WithMockUser
+	public void basicTemplateShouldReturnAppName() throws Exception {
+		Page<Load> loadPage = Mockito.mock(Page.class);
+		when(loadService.findPaginated(Mockito.any())).thenReturn(loadPage);
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Transport Market")));
+	}
+
+}
---
+++ src/test/java/pl/witoldbrzezinski/transportmarket/controller/LoadControllerTest.java
@@ -0,0 +1,23 @@
+package pl.witoldbrzezinski.transportmarket.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class LoadControllerTest {
+	
+	@Autowired
+	private LoadController loadController;
+		
+	@Test
+	public void controllerShouldNotBeNull() {
+		assertThat(loadController).isNotNull();
+	}
+	
+
+}
